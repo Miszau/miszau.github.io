f8f85643d9f1a29abe6617b6bcea5b297a008344
Miszau <micha.kurasinski@gmail.com>
2020-04-26 01:25:10 +0200

    asd

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <link rel = "stylesheet" href = "styles/styles.css">
+        <script src = "src/scripts.js"></script>
+    </head>
+
+    <body>
+        <div id = "header">
+            Welcome to my site - I am going to tell you everything you need 
+            to know about me so get ready
+        </div>
+
+
+    </body>
+
+</html>
+
+<?php
+
+echo "hello world";
+
+?>
